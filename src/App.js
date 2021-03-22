f3e9e250795a170c438bd4af0b7f301bbaecf5f1
Amen Moja Ra <mojaray2k@gmail.com>
2021-03-21 21:36:35 -0400

    Update App.js
    Add more text

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <h1>My First App</h1>
-      <p>This is my  first time <b>coding</b></p>
+      <p>This is my  first time <b>coding</b> and pushing code.</p>
       <a href="https://amazon.com">Link to Amazon</a>
         <img style={{width:'400px', height:'200px' }} src="https://miro.medium.com/max/3960/0*HICLyAdNSIyT0ODU.jpg" alt="This is an images of a developer"/>      
     </div>
