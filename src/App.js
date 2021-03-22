2140f0bb987f70e0649035771481f460b05682ed
Amen Moja Ra <mojaray2k@gmail.com>
2021-03-21 21:49:00 -0400

    Update App.js
    Here is my example of html attributes

--- src/App.js
+++ src/App.js
@@ -3,10 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>My First App</h1>
-      <p>This is my  first time <b>coding</b> and pushing code.</p>
-      <a href="https://amazon.com">Link to Amazon</a>
-        <img style={{width:'400px', height:'200px' }} src="https://miro.medium.com/max/3960/0*HICLyAdNSIyT0ODU.jpg" alt="This is an images of a developer"/>      
+      <a href="https://www.w3schools.com">Visit W3Schools</a>
+      <img src="https://www.w3schools.com/html/img_girl.jpg" alt="Girl with a jacket" />
+      <p style={{color:'red'}}>This is a red paragraph.</p>
     </div>
   );
 }
